fe9cf4e1ff391120b00d6eb4efcc602c33888efe
Bharath Pantala <bharathpantala@gmail.com>
2019-04-17 13:02:26 +0000

    script to run .py code

--- movie.sh
+++ movie.sh
@@ -3,3 +3,4 @@ echo "Enter movie name:"
 read movie
                                                                                                                                                     
 python /usr/src/app/omdbapi.py -t $movie --tomatoes
+
