f1f274619b812667468214e2ed99ec7bbaabbba8
bharath <bharath.pantala@yahoo.in>
2019-04-17 15:28:20 +0530

    2.5
    OMDB API CALL

--- omdbapi.py
+++ omdbapi.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# ----------OMDB_API_QUERY----------------
+# ---------------------OMDB_MOVIE_SEARCH----------------------------#
 
 from __future__ import print_function
 import argparse
